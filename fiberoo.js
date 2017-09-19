52cc7f38007fc42369c3386262cd5ef8baa06366
Blake Wallick <bwallick@hawaii.edu>
2017-09-19 10:00:26 -1000

    At explanation

---
+++ fiberoo.js
@@ -0,0 +1,25 @@
+function fib(maxNum) {
+  let base1 = 0;
+  let base2 = 1;
+  let temp = 0;
+  const answer = [];
+
+  answer.push(0);
+  answer.push(1);
+
+  for (let i = 2; i < maxNum; i++) {
+    temp = base1 + base2;
+    base1 = base2;
+    base2 = temp;
+    answer.push(temp);
+  }
+  return answer;
+}
+
+const maxNum = 100;
+
+console.log(fib(maxNum));
+
+function numsToStrings(answer) {
+  _.map(answer, function() {return })
+}
